827eff908544f4538b3228bc4bc9f8df16f6b462
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 04:02:04 +0700

    memberi spasi dan menambahkan text universitas muhamadiyah

--- app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
+++ app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
@@ -8,14 +8,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quest6_128.R
 
 @Composable
@@ -44,3 +48,11 @@ fun MahasiswaFormView(
                 contentDescription = "",
                 modifier = Modifier.size(45.dp)
             )
+            Spacer(modifier = Modifier.padding(4.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhamadiyah Yogyakarta",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
