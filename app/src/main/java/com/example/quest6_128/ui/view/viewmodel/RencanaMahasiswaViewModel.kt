04c8ae78bd5ea60f72dd1d426328a636ee76f5a2
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 04:42:42 +0700

    Membuat Class Rencana Mahasiswa View Model

---
+++ app/src/main/java/com/example/quest6_128/ui/view/viewmodel/RencanaMahasiswaViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.quest6_128.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.quest6_128.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaMahasiswaViewModel: ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMatakuliah(mkPilihan: String) {
+        _krsState.update { stateMk -> stateMk.copy(namaMk = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>) {
+        if (ls.size >= 2) {
+            _krsState.update { status ->
+                status.copy(
+                    namaMk = ls[0],
+                    kelas = ls[1]
+                )
+            }
+        } else {
+            println("Data tidak valid. Pastikan list memiliki minimal 2 elemen.")
+        }
+    }
+}
