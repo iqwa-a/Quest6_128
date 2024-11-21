1f8c1cb25c23e51649f31c829989d1fb3ac1b54d
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 03:36:07 +0700

    Membuat Object Matakuliah

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.quest6_128"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.quest6_128"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/example/quest6_128/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.quest6_128.data
+
+object MataKuliah {
+    val option = listOf(
+        "Pemprograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer vision"
+    )
+}
