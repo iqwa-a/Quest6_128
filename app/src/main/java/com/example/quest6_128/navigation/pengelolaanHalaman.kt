dc408a9e7ee4703106b802dff34f64cb2c38f4e8
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 03:45:20 +0700

    untuk menyimpan data mahasiswa

--- app/src/main/java/com/example/quest6_128/navigation/pengelolaanHalaman.kt
+++ app/src/main/java/com/example/quest6_128/navigation/pengelolaanHalaman.kt
@@ -1,8 +1,32 @@
 package com.example.quest6_128.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.quest6_128.model.Mahasiswa
+
 enum class Halaman {
     MahasiswaForm,
     RencanaStudy,
     Splash,
     Tampil,
 }
+@Composable
+fun PengelolaanHalaman(
+    modifier: Modifier = Modifier,
+    navHost: NavHostController = rememberNavController()
+) {
+    val mahasiswa = remember {
+        mutableStateOf(
+            Mahasiswa(
+                Nama = "",
+                Nim = "",
+                Email = "",
+                Matakuliah = "",
+                Kelas = ""
+            )
+        )
+    }
