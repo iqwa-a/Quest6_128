90650041673f928c5d94bce82d126e918f9e8d47
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 03:58:21 +0700

    membuat Colum

--- app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
+++ app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
@@ -1,8 +1,14 @@
 package com.example.quest6_128.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 
 @Composable
 fun MahasiswaFormView(
@@ -12,3 +18,14 @@ fun MahasiswaFormView(
     val nama = remember { mutableStateOf("") }
     val nim = remember { mutableStateOf("") }
     val email = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
