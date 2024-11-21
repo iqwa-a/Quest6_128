aebb81338db20deb9f865d732efba3e53380c6ee
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 04:13:00 +0700

    membuat Scaffold

--- app/src/main/java/com/example/quest6_128/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/quest6_128/ui/view/screen/RencanaStudyView.kt
@@ -1,5 +1,11 @@
 package com.example.quest6_128.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -7,6 +13,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.quest6_128.model.Mahasiswa
 
 @Composable
 fun RencanaStudyView(
@@ -25,3 +34,16 @@ fun RencanaStudyView(
     val snackbarHostState = remember { SnackbarHostState() }
 
     val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = colorResource(id = R.color.primary))
+                .padding(paddingValues)
+        ){
+
+
+
