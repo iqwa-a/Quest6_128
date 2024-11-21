ae22755d8ba441b682cea244ffb8a45482e23a0a
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 03:56:44 +0700

    Membuat Composable MahasiswaFromView

---
+++ app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
@@ -0,0 +1,9 @@
+package com.example.quest6_128.ui.view.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MahasiswaFormView(
+    onSubmitButtonCliked: (MutableList<String>) -> Unit,
+    onBackButtonCliked: () -> Unit,
+)
