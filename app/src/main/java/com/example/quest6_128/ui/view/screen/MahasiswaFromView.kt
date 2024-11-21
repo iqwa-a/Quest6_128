1b5164748f92a06d10af1d522d898d6e7732ae2a
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 04:00:07 +0700

    membuat spacer Row dan memberi gambar

--- app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
+++ app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
@@ -1,17 +1,22 @@
 package com.example.quest6_128.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.quest6_128.R
 
 @Composable
 fun MahasiswaFormView(
@@ -33,3 +38,9 @@ fun MahasiswaFormView(
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Spacer(modifier = Modifier.padding(top = 16.dp))
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.umy2),
+                contentDescription = "",
+                modifier = Modifier.size(45.dp)
+            )
