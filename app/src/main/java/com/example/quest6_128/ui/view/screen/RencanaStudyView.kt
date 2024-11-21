6386eb3f0ccf9497aa0a6bf07c2236d77a35dda4
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 04:17:08 +0700

    beri spasi dan tambah test

--- app/src/main/java/com/example/quest6_128/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/quest6_128/ui/view/screen/RencanaStudyView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -19,9 +20,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quest6_128.model.Mahasiswa
 
 @Composable
@@ -62,6 +66,21 @@ fun RencanaStudyView(
                     modifier = Modifier
                         .size(50.dp)
                 )
+                Spacer(modifier = Modifier.padding(start = 16.dp))
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = mahasiswa.Nim,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = Color.White
+                    )
+                    Text(
+                        text = mahasiswa.Nama,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 15.sp,
+                        color = Color.White
+                    )
+                }
 
 
 
