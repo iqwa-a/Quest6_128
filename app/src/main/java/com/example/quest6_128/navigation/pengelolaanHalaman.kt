6de4e9430594e3354f8ed972145bab475cbb0a3f
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 03:45:59 +0700

    mengedit navhost

--- app/src/main/java/com/example/quest6_128/navigation/pengelolaanHalaman.kt
+++ app/src/main/java/com/example/quest6_128/navigation/pengelolaanHalaman.kt
@@ -1,10 +1,12 @@
 package com.example.quest6_128.navigation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.quest6_128.model.Mahasiswa
 
@@ -30,3 +32,9 @@ fun PengelolaanHalaman(
             )
         )
     }
+
+    NavHost(
+        modifier = modifier.fillMaxSize(),
+        navController = navHost,
+        startDestination = Halaman.Splash.name
+    )
