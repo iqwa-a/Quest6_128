4f4167d3a9ad4d872828c2a45bd26be36ee76331
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 03:57:42 +0700

    Membuat mutablestate val nama nim email

--- app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
+++ app/src/main/java/com/example/quest6_128/ui/view/screen/MahasiswaFromView.kt
@@ -1,9 +1,14 @@
 package com.example.quest6_128.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 @Composable
 fun MahasiswaFormView(
     onSubmitButtonCliked: (MutableList<String>) -> Unit,
     onBackButtonCliked: () -> Unit,
-)
+) {
+    val nama = remember { mutableStateOf("") }
+    val nim = remember { mutableStateOf("") }
+    val email = remember { mutableStateOf("") }
