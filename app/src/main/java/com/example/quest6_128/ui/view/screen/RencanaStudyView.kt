49682fb3c93a7ff7fcfa3d4968494030acbdc896
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-11-22 04:14:03 +0700

    membuat Row Yang berisi Image

--- app/src/main/java/com/example/quest6_128/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/quest6_128/ui/view/screen/RencanaStudyView.kt
@@ -1,9 +1,13 @@
 package com.example.quest6_128.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -13,8 +17,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.quest6_128.model.Mahasiswa
 
 @Composable
@@ -44,6 +51,17 @@ fun RencanaStudyView(
                 .background(color = colorResource(id = R.color.primary))
                 .padding(paddingValues)
         ){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.umy2),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(50.dp)
+                )
 
 
 
